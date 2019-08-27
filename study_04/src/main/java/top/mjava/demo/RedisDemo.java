2774b5544bc74312c750a04f7db3fb404b75bac4
z573419235 <573419235@qq.com>
2019-08-27 15:34:14 +0800

    1

--- study_04/src/main/java/top/mjava/demo/RedisDemo.java
+++ study_04/src/main/java/top/mjava/demo/RedisDemo.java
@@ -1,6 +1,14 @@
 package top.mjava.demo;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import redis.clients.jedis.Jedis;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.util.HashMap;
 
 /**
  * redis demo
@@ -8,7 +16,24 @@ import redis.clients.jedis.Jedis;
  * */
 public class RedisDemo {
     public static void main(String[] args) {
-        Jedis jedis=new Jedis("localhost",6379);
-        System.out.println(jedis.hget("runoob","fiele1"));
+
+
+        try {
+            Jedis jedis=new Jedis("localhost",6379);
+            HashMap<String,String> errorList= (HashMap<String, String>) jedis.hgetAll("errorList");
+            JSONObject json= JSON.parseObject(JSON.toJSONString(errorList));
+            System.out.println(json.get("3df480fb50cf4b8d95e29fc2b8942d02"));
+            File f=new File("/home/zheng/errorlist2.json");
+            f.createNewFile();
+            BufferedWriter bufferedWriter=new BufferedWriter(new FileWriter(f));
+            bufferedWriter.write(json.toJSONString());
+            bufferedWriter.flush();
+            bufferedWriter.close();
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }finally {
+
+        }
+
     }
 }
